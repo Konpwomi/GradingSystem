9eacffe18a3b046b6b6e9a4cf2ed16c7c5405268
Patiphan Arphorn <126466338+Konpwomi@users.noreply.github.com>
2023-10-19 19:15:08 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace GradingSystem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter score: ");
+            string input = Console.ReadLine();
+
+            if (Regex.IsMatch(input, @"[^0-9abc]"))
+            {
+                Console.WriteLine("Please type only numbers, Try again.");
+            }
+            else
+            {
+                int score = Convert.ToInt32(input);
+                Grading.GradeCalculator(score);
+            }
+        }
+    }
+}
